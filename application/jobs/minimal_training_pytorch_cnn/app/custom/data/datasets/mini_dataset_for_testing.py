c3879835a25d50dbb9ae5a53658c6aeffd31c34a
klarabracke <klara@ju60.de>
2025-05-19 09:01:37 +0200

    update dataset

--- application/jobs/minimal_training_pytorch_cnn/app/custom/data/datasets/mini_dataset_for_testing.py
+++ application/jobs/minimal_training_pytorch_cnn/app/custom/data/datasets/mini_dataset_for_testing.py
@@ -6,25 +6,36 @@ import torch.utils.data as data
 class MiniDatasetForTesting(data.Dataset):
     def __init__(self):
         num_entries = 10
-        self.data = [{'uid': str(uuid.uuid4()), 'source': self.dummy_image(index), 'target': index % 2} for index in range(num_entries)]
+        self.data = [
+            {
+                'uid': str(uuid.uuid4()),
+                'source': self.dummy_image(index),
+                'target': index % 2
+            }
+            for index in range(num_entries)
+        ]
 
     @staticmethod
     def dummy_image(index):
         shape = (1, 18, 18)
-        dtype = np.float16
+        dtype = np.float16    
         if index % 2 == 0:
             array = np.zeros(shape, dtype=dtype)
             array[0, 0, index] = 1
         else:
             array = np.ones(shape, dtype=dtype)
             array[0, 0, index] = 0
-        return torch.from_numpy(array)
+       
+        return torch.from_numpy(array).float()
 
     def __getitem__(self, index):
-        return self.data[index]
+        sample = self.data[index].copy()
+       
+        sample['source'] = sample['source'].float()
+        return sample
 
     def __len__(self):
         return len(self.data)
 
     def get_labels(self):
-        return[i['target'] for i in self.data]
+        return [i['target'] for i in self.data]
